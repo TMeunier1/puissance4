9e2efec29ebc7baa4c1b0b59b5d09d8374b01efd
Tibau <xiliar1888@hotmail.fr>
2017-02-09 17:01:00 +0100

    Ajout de joueurs avec leur couleur et changement class div au clic

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 var rowsNumber = 6;
 var columnNumber = 7;
 var boardElement = document.getElementById("board");
+var joueurUn = 1;
 for (var i = 0; i < columnNumber; i++) {
     for (var j = 0; j < rowsNumber; j++) {
         var grid = document.createElement("div");
         grid.className = "tableau";
         boardElement.appendChild(grid);
+        grid.addEventListener ("click", function() {
+            if (joueurUn == 1) {
+                this.className = "tableau";
+                this.className = "playerone";
+                joueurUn = 0;
+            }
+            else {
+                this.className = "playertwo";
+                joueurUn = 1;
+            }
+        });
     }
 }
 
@@ -16,5 +28,3 @@ var boardElements = [["","","","","","",""],
                      ["","","","","","",""],
                      ["","","","","","",""]
                     ];
-var red = "";
-var yellow = "";
