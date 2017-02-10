a0df186f01da4f5f9f8ab8e1336f473deb132868
Tibau <xiliar1888@hotmail.fr>
2017-02-10 13:33:49 +0100

    ajout de grid, changement id etc

--- js/script.js
+++ js/script.js
@@ -1,30 +1,67 @@
+var grid =  [[0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0]];
 var rowsNumber = 6;
 var columnNumber = 7;
+var buttonNumber = 7;
 var boardElement = document.getElementById("board");
+var bodyElement = document.getElementById("body")
 var joueurUn = 1;
-for (var i = 0; i < columnNumber; i++) {
-    for (var j = 0; j < rowsNumber; j++) {
-        var grid = document.createElement("div");
-        grid.className = "tableau";
-        boardElement.appendChild(grid);
-        grid.addEventListener ("click", function() {
-            if (joueurUn == 1) {
-                this.className = "tableau";
-                this.className = "playerone";
+
+    for (var i = 0; i < rowsNumber; i++) {
+
+        for (var j = 0; j < columnNumber; j++) {
+            var divElement = document.createElement("div");
+            divElement.className = "tableau";
+            boardElement.appendChild(divElement);
+            divElement.id = "case"+i+"-"+j;
+        }
+    }
+    for (var k = 0; k < buttonNumber; k++) {
+        var button = document.createElement("button");
+        bodyElement.appendChild(button);
+        button.className = "button";
+        button.id = k;
+
+        button.addEventListener ("click", function() {
+           var col = this.id;
+           var i = 0;
+           while(i < 6 && grid[i][col] == 0) {
+               i++;
+           }
+           var lig = i-1;
+             if (joueurUn == 1) {
+                //document.getElementById("case"+col).className = "playerone";
                 joueurUn = 0;
-            }
-            else {
-                this.className = "playertwo";
+                grid[lig][col] = 1;
+              }
+
+             else {
                 joueurUn = 1;
-            }
-        });
+                grid[lig][col] = 2;
+             }
+         });
+    }
+
+function afficherGrille (){
+    for (var i = 0; i < grid.length; i++){
+        console.log(grid[i]);
     }
 }
 
-var boardElements = [["","","","","","",""],
-                     ["","","","","","",""],
-                     ["","","","","","",""],
-                     ["","","","","","",""],
-                     ["","","","","","",""],
-                     ["","","","","","",""]
-                    ];
+function actualiserGrille (){
+    for(i = 0; i <  grid.length; i++){
+        for(j = 0; i < grid[i].length; i++) {
+            if (grid[i] == 1) {
+                document.getElementById("case"+col+"-"+lig).className = "playerone";
+            }
+            else if (grid[i] == 2) {
+                document.getElementById("case"+col+"-"+lig).className = "playertwo";
+            }
+        }
+    }
+
+}
